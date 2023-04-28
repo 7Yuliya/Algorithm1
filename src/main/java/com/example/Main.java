b86c598ebde063b3a425436ad90bbf28fad6ecf3
Юлия Алексина <uliyaalexina@mail.ru>
2023-04-27 20:45:28 -0700

    Added src/

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int х = 9;
+        if (х > 7) {
+            System.out.println("Привет");
+        }
+    }
+}
